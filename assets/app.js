142e1f2c6f3fbea9da711099e350b9d770764974
Brian Elliston <brianelliston@Brians-MacBook-Pro.local>
2017-12-16 10:11:15 -0500

    working on it

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,32 @@
 alert("moving forward")
+
+var boats = ["Yacht", "Dinghy", "Sail", "Power"];
+
+var queryURL = "http://api.giphy.com/v1/gifs/search?" + boats + "api_key=6rYpWtEtGmNhbjcdGWby5Lq1FZETiUE3"
+
+    
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(response);
+    });
+
+
+     // Looping through the array of movies
+     for (var i = 0; i < boats.length; i++) {
+      
+                // Then dynamicaly generating buttons for each movie in the array.
+                // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+                var a = $("<button>");
+                // Adding a class
+                a.addClass("boat");
+                // Adding a data-attribute with a value of the movie at index i
+                a.attr("data-name", boats[i]);
+                // Providing the button's text with a value of the movie at index i
+                a.text(boats[i]);
+                // Adding the button to the HTML
+                $("#boats-view").append(a);
+              }
+            
